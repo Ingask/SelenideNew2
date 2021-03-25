02121db0a2b1afbb2526ac4daf000ddf1fbf617d
Inga Oberemchuk <inga_oberemchuk@mail.ru>
2021-03-25 08:40:10 +0300

    Modified #2

--- src/test/java/ru/netology/AppCardDeliveryTest.java
+++ src/test/java/ru/netology/AppCardDeliveryTest.java
@@ -7,28 +7,27 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.withText;
+//import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class AppCardDeliveryTest {
     @Test
     void trueInputCardDeliveryAppTest(){
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Москва");
+        $("[data-test-id = 'city'] input").setValue("Москва");
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DAY_OF_MONTH, 5);
-        SimpleDateFormat rusDateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String dateMeeting = rusDateFormat.format(calendar.getTime());
-        SelenideElement dateElement = $("[data-test-id=date] input[class=input__control]");
+        SimpleDateFormat formDateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String chooseDate = formDateFormat.format(calendar.getTime());
+        SelenideElement dateElement =  $("[data-test-id=date] input[class=input__control]");
         dateElement.sendKeys("\b\b\b\b\b\b\b\b\b\b");
-        dateElement.setValue(dateMeeting);
+        dateElement.setValue(chooseDate);
         $("[data-test-id=name] input").setValue("Иванов Иван");
-        $("[data-test-id=phone] input").setValue("+71234561230");
+        $("[data-test-id=phone] input").setValue("+71234567890");
         $("[data-test-id=agreement]").click();
         $$("button").find(exactText("Забронировать")).click();
-        $("[data-test-id=notification]")
-                .$(withText("Встреча успешно забронирована на"))
-                .waitUntil(visible, 15000);
+        $("[data-test-id=notification]").waitUntil(visible, 15000)
+                .shouldHave(text("Встреча успешно забронирована на"));
 
     }
 }
